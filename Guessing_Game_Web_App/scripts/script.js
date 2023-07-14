afc5ce75c76eb419dcf325e08eafe4d291961161
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2023-07-14 22:59:09 +0800

    update

---
+++ Guessing_Game_Web_App/scripts/script.js
@@ -0,0 +1,38 @@
+function addPosition(x, number) {
+
+
+    console.log("Submit Clicked")
+    console.log(number);
+    console.log(x);
+    var pos = document.getElementById("position");
+    while (pos.classList.length > 0) {
+        pos.classList.remove(pos.classList.item(0));
+    }
+    if (number == x) {
+        pos.classList.add("correct");
+        console.log("correct!")
+        alert("correct!")
+    } else if (number > x - 10 && number !== x && number < x + 10 && number !== x) {
+        pos.classList.add("veryClose");
+        console.log("Very Close")
+    } else if (number < x - 20 && number > x + 20) {
+        pos.classList.add("far");
+        console.log("Far")
+    } else {
+        pos.classList.add("veryFar");
+        console.log("Very Far!")
+
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let x = 50;
+    var button = document.getElementById("submitButton");
+    button.addEventListener("click", () => {
+        let number = document.getElementById("inputNumber").value;
+        console.log("submitted: " + number);
+        addPosition(x, number);
+    });
+});
