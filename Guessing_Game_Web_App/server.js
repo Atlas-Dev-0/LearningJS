aaf9c7fc16757cfed12289cd6318ba39beb03f1a
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2023-07-16 19:15:00 +0800

    added server.js

---
+++ Guessing_Game_Web_App/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Read the index.html file
+  fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
+    if (err) {
+      // If there's an error reading the file, send a 500 response
+      res.writeHead(500);
+      res.end('Error loading index.html');
+    } else {
+      // Send a 200 OK response with the contents of index.html
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
+});
+
+const port = 3000; // Choose the port you want to use
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
